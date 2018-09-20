9fae064c5a505c80a165d9024ee6fb65e08e6a59
Jay Houn <jayhoun@gmail.com>
2018-09-20 13:54:47 -1000

    created homepage component

--- App.js
+++ App.js
@@ -1,28 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
 import { createStackNavigator } from 'react-navigation';
+import Homepage from './Homepage';
 import PledgePage from './PledgePage';
-
-class Homepage extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Pledge</Text>
-        <Text style={styles.paragraph}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </Text>
-        <Button
-          title="Pledge Now"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
-}
 
 const RootStack = createStackNavigator(
   {
@@ -39,20 +18,3 @@ export default class App extends React.Component {
     return <RootStack />;
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  title: {
-    fontSize: 30,
-    paddingBottom: 100
-  },
-  paragraph: {
-    width: 250,
-    textAlign: 'justify',
-    paddingBottom: 100
-  }
-});
---
+++ Homepage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+export default class Homepage extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Pledge</Text>
+        <Text style={styles.paragraph}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </Text>
+        <Button
+          title="Pledge Now"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 30,
+    paddingBottom: 100
+  },
+  paragraph: {
+    width: 250,
+    textAlign: 'justify',
+    paddingBottom: 100
+  }
+});

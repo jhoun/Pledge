44240193c184e346796c95074e65e975c2837a7e
Jay Houn <jayhoun@gmail.com>
2018-09-20 23:12:49 -1000

    added swipe functionality on pledgepage

--- PledgePage.js
+++ PledgePage.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import Swiper from 'react-native-swiper';
+import data from './data/pledge-data';
 
 export default class PledgePage extends React.Component {
   render() {
+    console.log('data', data);
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <Text style={styles.header}>Hello</Text>
+        <Swiper style={styles.wrapper}>
+          <View style={styles.slide1}>
+            <Text style={styles.text}>Hello Swiper</Text>
+          </View>
+          <View style={styles.slide2}>
+            <Text style={styles.text}>Beautiful</Text>
+          </View>
+        </Swiper>
+        <View style={styles.button}>
+          <Button title="click me" />
+        </View>
       </View>
     );
   }
@@ -17,5 +32,36 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  header: {
+    fontSize: 50
+  },
+  wrapper: {},
+  slide1: {
+    height: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red'
+  },
+  slide2: {
+    height: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BB'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 100,
+    fontWeight: 'bold'
+  },
+  button: {
+    paddingTop: 20,
+    flex: 1
   }
 });
---
+++ data/pledge-data.js
@@ -0,0 +1,6 @@
+const pledgeData = [
+  { pledge: 'Stop using plastic straws' },
+  { pledge: 'Stop using plastic containers' }
+];
+
+export default pledgeData;
